ef494132f8587e61c476acc51dfb32e58a0fe194
jchuru <jameschuru123@gmail.com>
2020-03-10 17:15:57 -0400

    Finished all but sort

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,155 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import java.util.Iterator;
+
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<T>{
+
+    Node<T> head;
+    Node<T> tail;
+
+    public SinglyLinkedList(){
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void add(T value){
+
+        if(head == null){
+            head = new Node<T>(value);
+            tail = head;
+        }else{
+
+            Node noddy = new Node(value);
+            tail.setNext(noddy);
+            tail = noddy;
+        }
+
+    }
+    public T remove(Integer index){
+
+        Node<T> current;
+        Node<T> next;
+        T removed = null;
+
+        if(index == 0){
+            current = head;
+            head = current.getNext();
+        }else if(index == getSize(this.head)){
+
+            int count = 0;
+            for(current = head; count < getSize(this.head) ; current = current.getNext()){
+
+                if(count == getSize(this.head) -1){
+                    this.tail = current;
+                    this.tail.setNext(null);
+                }
+                count++;
+            }
+
+        }else{
+            Node<T> valToRemove = null;
+            int count = 0;
+            for(current = head; count < index ; current = current.getNext()){
+                if(count == index -1){
+                    valToRemove = current.getNext();
+                    removed = valToRemove.getValue();
+                    next = valToRemove.getNext();
+                    current.setNext(next);
+                    return removed;
+                }
+                count++;
+
+            }
+        }
+        return removed;
+    }
+    public Integer getSize(Node head){
+        Integer size = 0;
+        Node current = head;
+        while(current != null){
+            current = current.getNext();
+            size++;
+        }
+        return size;
+    }
+    public Node getHead(){
+        return this.head;
+    }
+
+    public T getValue(Integer index){
+
+        Node<T> current = null;
+        int count = 0;
+        for(current = head; count < index; current = current.getNext()){
+            count++;
+        }
+        return current.getValue();
+    }
+    // ******************************  CONTAINS  *************
+    public boolean contains(T value){
+        if(find(value) == -1){
+            return false;
+        }else{
+            return true;
+        }
+    }
+    // *****************************  FIND  ******************
+    public Integer find(T value){
+        Integer index = -1;
+        Node<T> current = null;
+        int count = 0;
+        for(current = head; count < getSize(this.head); current = current.getNext()){
+            if(current.getValue() == value){
+                index = count;
+                return index;
+            }
+            count++;
+        }
+        return index;
+    }
+    // *******************************     COPY    ****************
+    public SinglyLinkedList<T> copy(){
+        return this;
+    }
+    // *******************************    sort     ****************
+    public SinglyLinkedList<T> sort(){
+        return null;
+    }
+
+
+    // ********************************    NODE CLASS    ***********************
+    static class Node<T>{
+
+        T value;
+        Node<T> next;
+
+        public Node(T newValue , Node<T> nextNode){
+            this.value = newValue;
+            this.next = nextNode;
+        }
+        public Node(T val){
+            this.value =  val;
+            this.next = null;
+        }
+        public Node(){
+            this.value = null;
+            this.next = null;
+        }
+        public T getValue() {
+            return value;
+        }
+        public void setValue(T value) {
+            this.value = value;
+        }
+        public Node<T> getNext() {
+            return next;
+        }
+        public void setNext(Node<T> next) {
+            this.next = next;
+        }
+    }
 }
+
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,159 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.xml.soap.Node;
+
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedListTest {
+public class SinglyLinkedListTest<T> {
+
+
+    @Test
+    public void addAndSizeTest(){
+        SinglyLinkedList linky = new SinglyLinkedList();
+
+        Integer expected = 0;
+        Integer aatual = linky.getSize(linky.getHead());
+
+        Assert.assertEquals(expected , aatual);
+
+        String name = "Jimmt";
+        linky.add(name);
+
+        Integer expected1 = 1;
+        Integer aatual1 = linky.getSize(linky.getHead());
+
+        Assert.assertEquals(expected1 , aatual1);
+
+    }
+    @Test
+    public void removeFromMidTest(){
+
+        SinglyLinkedList<String> linky = new SinglyLinkedList<>();
+        Integer expected = 0;
+        Integer aatual = linky.getSize(linky.getHead());
+        Assert.assertEquals(expected , aatual);
+
+        linky.add("elem1");
+        linky.add("elem2");
+        linky.add("elem3");
+        linky.add("elem4");
+        linky.add("elem5");
+        linky.add("elem6");
+
+        // Before removing
+        Integer expectedSize = 6;
+        Integer actualSize = linky.getSize(linky.getHead());
+        Assert.assertEquals(expectedSize , actualSize);
+
+        //After removing
+        String valuue = linky.remove(3);                           // Removed "elem4" at index 3
+        Integer expectedSizeAfterremove = 5;                              // Size should be 1 ;ess
+        Integer actualsizeAfterRemove = linky.getSize(linky.getHead());
+        Assert.assertEquals(expectedSizeAfterremove , actualsizeAfterRemove);
+
+
+        String actualValue =  (String)linky.getValue(3); // element at index 3 - SHOUL NOW Be elem5
+        String expectedValue = "elem5";
+        Assert.assertEquals(expectedValue , actualValue);
+    }
+    @Test
+    public void removeFromSizeOfOneTest(){
+        SinglyLinkedList<String> linky = new SinglyLinkedList<>();
+        linky.add("elem1");
+
+        Integer expectedSize = 1;
+        Integer actualSize = linky.getSize(linky.getHead());
+        Assert.assertEquals(expectedSize , actualSize);
+
+        linky.remove(0);
+        Integer expectedSize2 = 0;
+        Integer actualSize2 = linky.getSize(linky.getHead());
+        Assert.assertEquals(expectedSize2 , actualSize2);
+    }
+    @Test
+    public void removeFromEndTest(){
+        SinglyLinkedList<String> linky = new SinglyLinkedList<>();
+        linky.add("elem1");
+        linky.add("elem2");
+
+        Integer expectedSize = 2;
+        Integer actualSize = linky.getSize(linky.getHead());
+        Assert.assertEquals(expectedSize , actualSize);
+
+        linky.remove(1);
+        Integer expectedSize2 = 1;
+        Integer actualSize2 = linky.getSize(linky.getHead());
+        Assert.assertEquals(expectedSize2 , actualSize2);
+
+        String actualValue = linky.getValue(0); // element at index 3 - SHOULD NOW Be elem5
+        String expectedValue = "elem1";
+        Assert.assertEquals(expectedValue , actualValue);
+
+    }
+    @Test
+    public void removeFromBeginingTest(){
+        SinglyLinkedList<String> linky = new SinglyLinkedList<>();
+        linky.add("elem1");
+        linky.add("elem2");
+
+        Integer expectedSize = 2;
+        Integer actualSize = linky.getSize(linky.getHead());
+        Assert.assertEquals(expectedSize , actualSize);
+
+        linky.remove(0);
+        Integer expectedSize2 = 1;
+        Integer actualSize2 = linky.getSize(linky.getHead());
+        Assert.assertEquals(expectedSize2 , actualSize2);
+    }
+    @Test
+    public void findTest(){
+        SinglyLinkedList<String> linky = new SinglyLinkedList<>();
+        linky.add("elem1");
+        linky.add("elem2");
+        linky.add("elem3");
+        linky.add("elem4");
+
+        Integer expectedIndex = 2;
+        Integer actualIndex = linky.find("elem3");
+        Assert.assertEquals(expectedIndex , actualIndex);
+
+        Integer expectedIndex1 = 0;
+        Integer actualIndex1 = linky.find("elem1");
+        Assert.assertEquals(expectedIndex1 , actualIndex1);
+
+        Integer expectedIndex2 = -1;
+        Integer actualIndex2 = linky.find("elem9");
+        Assert.assertEquals(expectedIndex2 , actualIndex2);
+    }
+    @Test
+    public void containsTest(){
+        SinglyLinkedList<String> linky = new SinglyLinkedList<>();
+        linky.add("elem1");
+
+        Assert.assertTrue(linky.contains("elem1"));
+        Assert.assertFalse(linky.contains("elem2"));
+    }
+    @Test
+    public void getValueTest(){
+        SinglyLinkedList<String> linky = new SinglyLinkedList<>();
+        linky.add("elem1");
+
+        String expected = "elem1";
+        String actual =  linky.getValue(0);
+        Assert.assertEquals(expected , actual);
+    }
+    @Test
+    public void copy(){
+        SinglyLinkedList<String> linky = new SinglyLinkedList<>();
+        linky.add("elem1");
+
+        SinglyLinkedList<String> linkyCloned =  linky.copy();
+
+        Assert.assertEquals(linky  , linkyCloned);
+    }
+
 }
